69361d99325c7b529907fd158c993eee55767062
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-10-12 21:22:07 +0900

    [change] unique

--- migrations/2021-10-10-122111_create_users/up.sql
+++ migrations/2021-10-10-122111_create_users/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     encrypted_password VARCHAR(255) NOT NULL
 );
